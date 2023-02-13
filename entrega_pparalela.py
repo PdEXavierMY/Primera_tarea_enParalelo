e3d5b4f684506bad37d1be71f2ca61a4401acb9c
Xavitheforce <javier.miguelez.yague@gmail.com>
2023-02-13 16:02:33 +0100

    Update entrega_pparalela.py

--- entrega_pparalela.py
+++ entrega_pparalela.py
@@ -1,3 +1,5 @@
+#comenzamos probando a ejecutar el primer ejemplo de codigo provisto
+
 urls = ["a.com", "b.com", "c.com", "d.com"]
 
 import random
@@ -9,44 +11,43 @@ def scrape(url):
     print("finished", url, "time taken:", duration, "seconds")
     return url, duration
 
-urls = ["a.com", "b.com", "c.com", "d.com"]
 output = []
 for url in urls:
     result = scrape(url)
     output.append(result)
 
-from multiprocessing import Pool
+#from multiprocessing import Pool
 
-pool = Pool(processes=4)
+# pool = Pool(processes=4)
 
-data = pool.map(scrape, urls)
+# data = pool.map(scrape, urls)
 
-scrape ("a.com") # hecho por el proceso 1 
-scrape ("b.com") # hecho por el proceso 2 
-scrape ("c.com") # hecho por el proceso 3 
-scrape ("d.com") # hecho por proceso 4
+# scrape ("a.com") # hecho por el proceso 1 
+# scrape ("b.com") # hecho por el proceso 2 
+# scrape ("c.com") # hecho por el proceso 3 
+# scrape ("d.com") # hecho por proceso 4
 
-pool.close()
-print()
-for row in data:
-    print(row)
+# pool.close()
+# print()
+# for row in data:
+#     print(row)
 
-urls = ["a.com", "b.com", "c.com", "d.com", "e.com"]
+# urls = ["a.com", "b.com", "c.com", "d.com", "e.com"]
 
-from multiprocessing import Pool
-from time import sleep
-import random
-def scrape(url):
-    print("starting", url)
-    duration = round(random.random(),3)
-    sleep(duration)
-    print("finished", url, "time taken:", duration, "seconds")
-    return url, duration
-urls = ["a.com", "b.com", "c.com", "d.com"]
-if __name__ == "__main__":
-    pool = Pool(processes=4)
-    data = pool.map(scrape, urls)
-    pool.close()    
-    print()
-    for row in data:
-        print(row)
+# from multiprocessing import Pool
+# from time import sleep
+# import random
+# def scrape(url):
+#     print("starting", url)
+#     duration = round(random.random(),3)
+#     sleep(duration)
+#     print("finished", url, "time taken:", duration, "seconds")
+#     return url, duration
+# urls = ["a.com", "b.com", "c.com", "d.com"]
+# if __name__ == "__main__":
+#     pool = Pool(processes=4)
+#     data = pool.map(scrape, urls)
+#     pool.close()    
+#     print()
+#     for row in data:
+#         print(row)
