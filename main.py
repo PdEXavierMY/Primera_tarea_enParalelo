3e98e1dc81c5fdca7431e6ee877f473dd7f8ae2b
Xavitheforce <javier.miguelez.yague@gmail.com>
2023-02-13 17:05:53 +0100

    main

---
+++ main.py
@@ -0,0 +1,4 @@
+from entrega_pparalela import ejecutar_practica #importamos la funcion ejecutar_practica del archivo entrega_pparalela.py
+
+if __name__ == "__main__": #si el archivo se ejecuta directamente, se ejecutara la funcion ejecutar_practica
+    ejecutar_practica()
